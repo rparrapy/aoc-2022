5785ead79427fc85255267bac7530bb24aed0809
Rodrigo Parra <rodpar07@gmail.com>
2022-12-24 09:59:03 -0300

    Add day 5 solution

---
+++ 5/main_2.py
@@ -0,0 +1,31 @@
+import re
+
+
+def parse_crate_line(line):
+    return [line[i] for i in range(1, len(line), 4)]
+
+def process_move(stacks, number_of_crates, from_stack, to_stack):
+    crates = stacks[from_stack][-number_of_crates:]
+    stacks[from_stack] = stacks[from_stack][:-number_of_crates]
+    stacks[to_stack] += crates
+    return stacks
+
+
+def main():
+    with open('input.txt') as f:
+        lines = f.readlines()
+        crate_rows = []
+        stacks = []
+        for line in lines:
+            if line.strip(" ").startswith("["):
+                crate_rows.append(parse_crate_line(line))
+            elif line.strip(" ").startswith("move"):
+                number_of_crates, from_stack, to_stack = re.findall(r'\d+', line)
+                stacks = process_move(stacks, int(number_of_crates), int(from_stack) - 1, int(to_stack) - 1)
+            else:
+                stacks = [[e for e in list(cr) if e != " "] for cr in zip(*reversed(crate_rows))]
+        print(stacks)
+        return "".join([s[-1] for s in stacks])
+
+if __name__ == "__main__":
+    print(main())
