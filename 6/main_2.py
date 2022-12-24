e64a7aea2b111ab6a1da7b02400ca3ea526aec71
Rodrigo Parra <rodpar07@gmail.com>
2022-12-24 10:12:55 -0300

    Add day 6 solution

---
+++ 6/main_2.py
@@ -0,0 +1,15 @@
+MARKER_LENGTH = 14
+
+def first_marker_pos(buffer):
+    for i in range(len(buffer) - (MARKER_LENGTH - 1)):
+        sub_buffer = buffer[i:i+MARKER_LENGTH]
+        if len(set(sub_buffer)) == MARKER_LENGTH:
+            return i + MARKER_LENGTH
+
+def main():
+    with open('input.txt') as f:
+        line = f.readlines()[0]
+        return first_marker_pos(line)
+
+if __name__ == "__main__":
+    print(main())
